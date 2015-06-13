dc05f3e89bd0a9dc9dbe8efd4625ab8e01ff81f7
michallorens <michallorens@gmail.com>
2015-06-14 00:58:18 +0200

    Pliki konfiguracyjne

---
+++ storm-kafka-server/src/main/java/pl/edu/agh/iosr/lambda/kafkastorm/SumAggregator.java
@@ -0,0 +1,28 @@
+package pl.edu.agh.iosr.lambda.kafkastorm;
+
+import storm.trident.operation.CombinerAggregator;
+import storm.trident.tuple.TridentTuple;
+
+public class SumAggregator implements CombinerAggregator<Double> {
+
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+    public Double init(TridentTuple tuple) {
+        return Double.parseDouble((String) tuple.getValue(0));
+    }
+
+    @Override
+    public Double combine(Double val1, Double val2) {
+        return val1 + val2;
+    }
+
+    @Override
+    public Double zero() {
+        return 0.;
+    }
+    
+}
